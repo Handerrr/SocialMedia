2d53d9519bfc11e3afba3c30e6add26967f16d15
Handerrr <hunterxxx2610@gmail.com>
2026-06-26 15:55:42 -0300

    testing

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -4,20 +4,30 @@ from decouple import config
 DEBUG = False
 
 ALLOWED_HOSTS = [
-".railway.app",
-".up.railway.app",
+    ".railway.app",
+    ".up.railway.app",
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'https://socialmedia-production-b6d8.up.railway.app',
+    "https://socialmedia-production-b6d8.up.railway.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://socialmedia-production-b6d8.up.railway.app',
+    "https://socialmedia-production-b6d8.up.railway.app",
 ]
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('CLOUD_NAME'),
-    'API_KEY': config('API_KEY'),
-    'API_SECRET': config('API_SECRET'),
+    "CLOUD_NAME": config("CLOUD_NAME"),
+    "API_KEY": config("API_KEY"),
+    "API_SECRET": config("API_SECRET"),
+}
+
+# WhiteNoise para produção
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
